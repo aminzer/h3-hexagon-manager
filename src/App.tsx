b8efe059e0c1cb5abb48e5a3a407f5c351cac31b
aminzer <minzer.alexey@gmail.com>
2025-12-21 00:04:07 +0300

    setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -23,14 +23,12 @@ function App() {
           layers: [
             new ScatterplotLayer({
               id: 'scatter-1',
-              data: [
-                { position: [-0.1278, 51.5074], size: 1000 }
-              ],
+              data: [{ position: [-0.1278, 51.5074], size: 1000 }],
               getPosition: (d: { position: [number, number]; size: number }) => d.position,
               getFillColor: [255, 0, 0, 100],
               getRadius: (d: { position: [number, number]; size: number }) => d.size,
-            })
-          ]
+            }),
+          ],
         });
         overlay.setMap(mapInstance.current);
       }
@@ -51,8 +49,11 @@ function App() {
   }, []);
 
   return (
-    <div ref={mapContainerRef} style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh' }} />
+    <div
+      ref={mapContainerRef}
+      style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh' }}
+    />
   );
 }
 
-export default App
+export default App;
